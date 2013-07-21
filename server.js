912a02ef060839835eb1cce144cf7c8295a4deb7
gensior <j.franceschini@gmail.com>
2013-07-21 03:22:09 -0700

    adds datetime and html formatting

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var http = require("http");
 
 http.createServer(function(request, response) {
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello World");
+	response.writeHead(200, {"Content-Type": "text/html"});
+	response.write("<html><body><h1>Hello World</h1>");
+	var datetime = new Date().toISOString();
+	response.write("<p>" + datetime + "</p>");
+	response.write("</body></html>")
 	response.end();
 }).listen(process.env.PORT || 8888);
