adf5058aa7c38b99d678ded0f7cec284a46e457c
gensior <j.franceschini@gmail.com>
2013-07-21 11:10:12 -0700

    formats datetime with moment

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var http = require("http");
+var moment = require("moment");
 
 http.createServer(function(request, response) {
 	response.writeHead(200, {"Content-Type": "text/html"});
 	response.write("<html><body><h1>Hello World</h1>");
-	var datetime = new Date().toISOString();
-	response.write("<p>" + datetime + "</p>");
+	response.write("<p>" + moment().format("dddd, MMMM Do YYYY, h:mm:ss a") + "</p>");
 	response.write("</body></html>")
 	response.end();
 }).listen(process.env.PORT || 8888);
