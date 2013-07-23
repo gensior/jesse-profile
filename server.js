50dbe05a0f2a6d237a768eaa50731aba5a34ad85
gensior <j.franceschini@gmail.com>
2013-07-22 17:12:56 -0700

    includes pacific DST zone.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var moment = require("moment");
 http.createServer(function(request, response) {
 	response.writeHead(200, {"Content-Type": "text/html"});
 	response.write("<html><body><h1>Hello World</h1>");
-	response.write("<p>" + moment().format("dddd, MMMM Do YYYY, h:mm:ss a") + "</p>");
+	response.write("<p>" + moment().zone("-07:00").format("dddd, MMMM Do YYYY, h:mm:ss a") + "</p>");
 	response.write("</body></html>")
 	response.end();
 }).listen(process.env.PORT || 8888);
