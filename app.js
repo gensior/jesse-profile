8c89ec079a64b3d740bd2857ff323cbc5a15c60d
gensior <j.franceschini@gmail.com>
2013-07-24 00:53:44 -0700

    Adds moment and uses templates.

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 var flatiron = require('flatiron'),
     path = require('path'),
     moment = require('moment'),
-    plate = require('plates'),
+    plates = require('plates'),
+    fs = require('fs'),
     app = flatiron.app;
 
 app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
 
+var template = fs.readFileSync("base.html", "utf-8");
+
 app.use(flatiron.plugins.http);
 
+// root GET
 app.router.get('/', function () {
 	var req = this.req,
 		res = this.res;
 
-	app.log.info('Saying hello!');
+	// app.log.info('Saying hello!');
 
 	res.writeHead(200, {'content-type': 'text/html'});
-	res.end('<html><body><h1>hello!</h1><p>' + moment().zone("-07:00").format("dddd, MMMM Do YYYY, h:mm:ss a") + '</p></body></html>');
+	res.end(plates.bind(template, {main: "<span id='datetime'>" + moment().zone("-07:00").format("dddd, MMMM Do YYYY, h:mm:ss a") + "</span>"}));
 });
 
 app.start(process.env.PORT || 8080, function (err) {
@@ -23,6 +27,6 @@ app.start(process.env.PORT || 8080, function (err) {
 		throw err;
 	}
 
-	var addr = app.server.address();
-	app.log.info('Listening on http://' + addr.address + ':' + addr.port);
+	// var addr = app.server.address();
+	// app.log.info('Listening on http://' + addr.address + ':' + addr.port);
 });
