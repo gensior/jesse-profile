a684e0af4bd70209305e29c9a460d3a55b1ba1ae
gensior <j.franceschini@gmail.com>
2013-07-27 04:42:44 -0700

    Utilizes vogels.

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+'use strict';
+
 var express = require('express'),
     moment = require('moment'),
     plates = require('plates'),
     fs = require('fs'),
-    AWS = require('aws-sdk'),
+    vogels = require('vogels'),
+    util = require('util'),
+    _ = require('lodash'),
     app = express();
 
-AWS.config.update({credentials: {accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_KEY}});
-AWS.config.update({region: 'us-west-2'});
-var db = new AWS.DynamoDB();
+vogels.AWS.config.update({accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_KEY});
+vogels.AWS.config.update({region: 'us-west-2'});
+// var db = new AWS.DynamoDB();
 
 var template = fs.readFileSync("templates/base.html", "utf-8");
 
@@ -20,26 +24,47 @@ app.get('/', function ( req, res ) {
 	console.log('root');
 });
 
+var Post = vogels.define('Post', function (schema) {
+	schema.Number('id', {hashKey: true});
+	schema.String('title').required();
+	schema.String('body');
+	schema.Date('created', {default: Date.now});
+	schema.UUID('key');
+});
+Post.config({tableName: 'posts'});
+
+var printResults = function (err, resp) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log('Found', resp.Count, 'items');
+		console.log(resp.Items[0].attrs);
+	    return resp;
+	}
+}
+
 // Test dynamoDB
 app.get('/dynamo', function (req, res) {
-	var body = "";
 	res.setHeader('Content-Type', 'text/html');
-	db.listTables(function (err, data) {
-		if (!err) {
-			var arr = data.TableNames;
-			var length = arr.length,
-				element = null;
-			for (var i=0; i<length; i++) {
-				element = arr[i];
-				body = body + element + '<br />';
-			}
-		} else {
-			body = err;
-			console.log(err);
-		}
-		// console.log(process.env);
-		res.end(plates.bind(template, {main: body}));
+	Post.get(1, function (err, post) {
+		return res.end(plates.bind(template, {main: post.get('title')}));
 	});
+	// db.listTables(function (err, data) {
+	// 	if (!err) {
+	// 		var arr = data.TableNames;
+	// 		var length = arr.length,
+	// 			element = null;
+	// 		for (var i=0; i<length; i++) {
+	// 			element = arr[i];
+	// 			body = body + element + '<br />';
+	// 		}
+	// 	} else {
+	// 		body = err;
+	// 		console.log(err);
+	// 	}
+	// 	// console.log(process.env);
+	// 	res.end(plates.bind(template, {main: body}));
+	// });
 });
 
 // static server
